95b4224ff396ba3004ced840ef8efa855312ef37
chirag mevada <chiragtech9teen@gmail.com>
2021-02-17 13:38:55 +0530

    commit from  mac

--- android/app/src/main/kotlin/com/itfuturz/smart_society_new/Application.kt
+++ android/app/src/main/kotlin/com/itfuturz/smart_society_new/Application.kt
@@ -10,10 +10,10 @@ class Application : FlutterApplication(), PluginRegistrantCallback {
 
     override fun onCreate() {
         super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
     }
 
     override fun registerWith(registry: PluginRegistry?) {
-        io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
+        io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin.registerWith(registry?.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"))
     }
 }
